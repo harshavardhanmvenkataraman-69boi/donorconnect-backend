56b24ffe212380c0473f715c9dc777899173ad65
Venkataraman <2478654@cognizant.com>
2026-04-15 17:06:03 +0530

    changed route for eureka

--- donorconnect/eureka-server/src/main/java/com/donorconnect/eurekaserver/EurekaServerApplication.java
+++ donorconnect/eureka-server/src/main/java/com/donorconnect/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.eurekaServer.demo;
+package com.donorconnect.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DemoApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
